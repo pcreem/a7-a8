a7d84dc6e69d7b45623250891e07c8f473cd1e43
pcreem <creempepper@gmail.com>
2019-11-25 16:15:09 +0800

    non

--- seeders/20191030072133-my-seed-file.js
+++ seeders/20191030072133-my-seed-file.js
@@ -39,7 +39,7 @@ module.exports = {
       ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
         .map((item, index) =>
           ({
-            // id: index + 1,
+            id: index + 1,
             name: item,
             createdAt: new Date(),
             updatedAt: new Date()
@@ -50,7 +50,7 @@ module.exports = {
     queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((d, i) =>
         ({
-          // id: i + 1,
+          id: i + 1,
           name: faker.name.findName(),
           tel: faker.phone.phoneNumber(),
           address: faker.address.streetAddress(),
@@ -69,7 +69,7 @@ module.exports = {
     return queryInterface.bulkInsert('Comments',
       [...Array(150)].map((item, index) => index).map(i =>
         ({
-          // id: i + 1,
+          id: i + 1,
           text: faker.lorem.sentence(),
           UserId: Math.floor(Math.random() * 3) + 1,
           RestaurantId: i % 50 + 1,
